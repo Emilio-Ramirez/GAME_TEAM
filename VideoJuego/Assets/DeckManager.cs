16f15fde35a8f6b1c88096b0de876b19037cadf7
Emilio Ramírez Mascarúa <emilio@Emilios-MacBook-Pro.local>
2024-04-20 11:04:36 -0600

    Agregue en cada grupo todos los sprites
    hice varios arrays para almacenar cada carta por tipo de evento y
    categoria,hay que nombrar las cartas para que no solo sean nombres con
    numeros.

--- VideoJuego/Assets/DeckManager.cs
+++ VideoJuego/Assets/DeckManager.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 public class DeckManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    // Boda event
+    public Sprite[] bodaProteinCards;
+    public Sprite[] bodaVegetableCards;
+    public Sprite[] bodaSideCards;
+    public Sprite[] bodaUtilCards;
+
+    // Navidad event
+    public Sprite[] navidadProteinCards;
+    public Sprite[] navidadVegetableCards;
+    public Sprite[] navidadSideCards;
+    public Sprite[] navidadUtilCards;
+
+    // Picnic event
+    public Sprite[] picnicProteinCards;
+    public Sprite[] picnicVegetableCards;
+    public Sprite[] picnicSideCards;
+    public Sprite[] picnicUtilCards;
     void Start()
     {
         
