9fcc561e60e6ef1d83be89b52494ba1ce9c0ae53
EC2 Default User <ec2-user@ip-172-31-40-2.us-east-2.compute.internal>
2024-05-27 03:33:27 +0000

    API DB Setup

---
+++ API_Web/API-AWS/db.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const pool = mysql.createPool({
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  database: process.env.RDS_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+module.exports = pool;
