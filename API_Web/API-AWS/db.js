3f462d8c02cc49a766cf1f210bf83f038d43bcfe
Sebas Tec <A01748052@tec.mx>
2024-05-27 00:45:03 -0600

    Prueba de API y cambios

--- API_Web/API-AWS/db.js
+++ API_Web/API-AWS/db.js
@@ -1,14 +1,26 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config();
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  database: process.env.RDS_DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
+
+pool.getConnection()
+  .then(connection => {
+    console.log("Connected to the database!");
+    connection.release();
+  })
+  .catch(error => {
+    console.error("Error connecting to the database:", error);
+  });
 
 module.exports = pool;
